98e3bfd3bd4dc0d4b42bd8aa53e4859f2c4ab5da
Marcelo Júnior <macelo17@gmail.com>
2023-06-17 12:14:53 -0300

    Esboço Final_Versão 1.3

--- main.c
+++ main.c
@@ -1,107 +1,101 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
+// Estrutura que representa uma conta bancária e suas funcionalidades básicas
 
-struct Conta {
-    float saldo;
-};
+typedef struct {
+    char tipo[20];
+    double saldo;
+} ContaBancaria;
 
-void deposito(struct Conta *conta, float valor) {
-    conta->saldo += valor;
-}
+// Função para solicitar um saque em uma conta bancária
 
-void sacar(struct Conta *conta, float valor) {
+bool sacar(ContaBancaria* conta, double valor) {
     if (conta->saldo >= valor) {
         conta->saldo -= valor;
-    printf("|*******************************************************|\n");
-	printf("|                                                       |\n");  
-    printf("| Seu saque foi realizado com sucesso!                  |\n");
-        } else {
-    printf("| Seu saldo eh insuficiente!                            |\n");
-    printf("|                                                       |\n");
-   	printf("|*******************************************************|\n");
-}
-    printf("|*******************************************************|\n");
+        return true;
+    } else {
+        return false;
     }
 }
 
-void exibirExtrato(struct Conta *conta, const char *tipoConta) {
-	
-    printf("|*******************************************************|\n");
-    printf("|                                                       |\n");    
-    printf("| Extrato detalhado de sua da Conta %s: %.2f            |\n", tipoConta, conta->saldo);
-	    if (conta->saldo > 0) {
-            
-    printf("| Saldo Positivo                                        |\n");
-  
-    } else {
+// Função para exibir o extrato da conta
 
-    printf("| Sem saldo disponivel                                  |\n");
-    printf("|                                                       |\n");
-    printf("|*******************************************************|\n");
+void exibirExtrato(ContaBancaria* contas, int numContas) {
+    printf("\n Extrato das contas:\n");
+    for (int i = 0; i < numContas; i++) {
+        printf("%s: R$%.2f reais\n", contas[i].tipo, contas[i].saldo);
     }
 }
 
 int main() {
-    struct Conta corrente, poupanca, salario;
+    // Criação das contas bancárias
+    ContaBancaria contas[3];
+    sprintf(contas[0].tipo, "Corrente");
+    contas[0].saldo = 1212.60;
+    sprintf(contas[1].tipo, "Salario");
+    contas[1].saldo = 2752.33;
+    sprintf(contas[2].tipo, "Poupanca");
+    contas[2].saldo = 1050.35;
 
-    corrente.saldo = 0.0;
-    poupanca.saldo = 0.0;
-    salario.saldo = 0.0;
+    while (true) {
+       
+        int opcao;
+        printf("\n Escolha uma opcao: \n");
+        printf("1. Realizar Saque \n");
+        printf("2. Exibir Extrato \n");
+        printf("3. Sair \n");
+        scanf("%d", &opcao);
 
-    deposito(&corrente, 0.00);
-    deposito(&poupanca, 5500.00);
-    deposito(&salario, 2752.00);
+        if (opcao == 1) {
+            // Opções de conta
+            int numConta;
+            printf("\n Escolha a conta para realizar o saque: \n");
+            printf("1. Conta Corrente\n");
+            printf("2. Conta Salario\n");
+            printf("3. Conta Poupanca\n");
+            scanf("%d", &numConta);
 
-    exibirExtrato(&corrente, "Corrente");
-    exibirExtrato(&poupanca, "Poupanca");
-    exibirExtrato(&salario, "Salario");
+            // Valor do saque
+            double valorSaque;
+            printf("Digite o valor que deseja sacar: ");
+            scanf("%lf", &valorSaque);
+            // Verifica se tem saldo na conta escolhida            
+            bool saldoSuficiente = false;
+            if (numConta >= 1 && numConta <= 3) {
+                if (sacar(&contas[numConta - 1], valorSaque)) {
+                    saldoSuficiente = true;
+                }
+            }
 
-    int opcaoConta;
-
-	do {
-	printf("|*******************************************************|\n");
-	printf("|                                                       |\n");
-	printf("|  Escolha qual a conta desejar realizar o saque:       |\n");
-    printf("|                                                       |\n");
-    printf("|  Escolha qual a conta desejar realizar o saque:       |\n");
-    printf("| 1 - Conta Corrente                                    |\n");
-    printf("| 2 - Conta Poupanca                                    |\n");
-    printf("| 3 - Conta Salario                                     |\n");
-    printf("| 4 - Sair da sua conta                                 |\n");
-    printf("|                                                       |\n");
-    printf("|*******************************************************|\n");
-    scanf("%d", &opcaoConta);
-  
-
-    if (opcaoConta == 4) {
-        break;
-    }
-
-
-    float valorSaque;
-    printf("Digite o valor que deseja sacar: ");
-    scanf("%f", &valorSaque);
-
-    switch (opcaoConta) {
-        case 1:
-            sacar(&corrente, valorSaque);
+            // Exibição do resultado
+            
+            if (saldoSuficiente) {
+                printf("Seu saque de R$%.2f reais foi realizado com sucesso na conta %s.\n \nRETIRE SEU DINHEIRO!\n", valorSaque, contas[numConta - 1].tipo);
+                printf("\a"); // Emitir som de bip
+            } else {
+                printf("Seu saldo eh insuficiente para realizar saques no valor de R$%.2f reais da %s.\n", valorSaque, contas[numConta - 1].tipo);
+            }
+        } else if (opcao == 2) {
+           
+		    // Exibição do extrato
+            exibirExtrato(contas, 3);
+        } else if (opcao == 3) {
+           
+		    // Comando de saída
+            printf("|*****************|\n");
+            printf("|                 |\n");
+            printf("| BANKofESTRELLA  |\n");
+            printf("|   OBRIGADO!     |\n");
+            printf("|                 |\n");
+            printf("|*****************|\n");
+            printf("\a"); //emitirar um bip sonoro no final
             break;
-        case 2:
-            sacar(&poupanca, valorSaque);
-            break;
-        case 3:
-            sacar(&salario, valorSaque);
-            break;
-        default:
-            printf(" Operacao invalida \n");
-    }
-
-    exibirExtrato(&corrente, "Corrente");
-    exibirExtrato(&poupanca, "Poupanca");
-    exibirExtrato(&salario, "Salario");
-
-	} while (1); 
-
-    return 0;
+	}else{
+		printf("\n Opcao invalida. Por favor, escolha uma opcao valida.\n");
+		printf("\a");
+	}
+}
+	return 0;
+	
 }
